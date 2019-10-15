e0522f727907c45c54f1e084e759b781e8c370d9
David Brownell <broda@microsoft.com>
2019-10-15 21:26:53 +0000

    Merged PR 5260: Added a mutex when modifying the underlying pointer table
    Added a mutex when modifying the underlying pointer table as ML.NET invokes featurizers across a number of different threads.

--- src/FeaturizerPrep/SharedLibrary/PointerTable.h
+++ src/FeaturizerPrep/SharedLibrary/PointerTable.h
@@ -3,48 +3,56 @@
 // Licensed under the MIT License
 // ----------------------------------------------------------------------
 #pragma once
-#include <unordered_map>
+
+#include <mutex>
 #include <random>
-#include <assert.h>
 #include <stdexcept>
+#include <unordered_map>
+
+#include <assert.h>
 
 namespace Microsoft{
 namespace Featurizer{
-    
+
 /////////////////////////////////////////////////////////////////////////
 ///  \class         PointerTable
 ///  \brief         Provide an interface to store pointers in exchange of
 ///                 an index to avoid exposing pointers to users
 ///
 class PointerTable {
-    public:
-        
+public:
     // ----------------------------------------------------------------------
     // |
     // |  Public Methods
     // |
     // ----------------------------------------------------------------------
-    
-        PointerTable(unsigned int seed = (std::random_device())());
+    PointerTable(unsigned int seed=(std::random_device())());
 
-        template<typename T> 
-        size_t Add(const T* const toBeAdded);
+    template<typename T>
+    size_t Add(const T* const toBeAdded);
 
-        template<typename T> 
-        T* Get(size_t index);
+    template<typename T>
+    T* Get(size_t index);
 
-        void Remove(size_t index);
+    void Remove(size_t index);
 
-    private:
+private:
+    // ----------------------------------------------------------------------
+    // |
+    // |  Private Types
+    // |
+    // ----------------------------------------------------------------------
+    using LockGuard                         = std::lock_guard<std::mutex>;
+
     // ----------------------------------------------------------------------
     // |
     // |  Private Data
     // |
     // ----------------------------------------------------------------------
-        std::unordered_map<std::size_t, const void*>   m_un;
-        std::mt19937 m_mt;
-
-    };
+    std::unordered_map<std::size_t, const void *>       m_un;
+    std::mutex                                          m_un_mutex;
+    std::mt19937                                        m_mt;
+};
 
 
 // ----------------------------------------------------------------------
@@ -56,36 +64,40 @@ class PointerTable {
 // ----------------------------------------------------------------------
 // ----------------------------------------------------------------------
 // ----------------------------------------------------------------------
-    inline PointerTable::PointerTable(unsigned int seed) : m_mt(seed) {
-        
+inline PointerTable::PointerTable(unsigned int seed) : m_mt(seed) {
+
+}
+
+
+template<typename T>
+size_t PointerTable::Add(const T * const templatePointer) {
+    if (templatePointer == nullptr) {
+        throw std::invalid_argument("Trying to add a null pointer to the table!");
     }
 
 
-    template<typename T>
-    size_t PointerTable::Add(const T * const templatePointer) {
-        if (templatePointer == nullptr) {
-            throw std::invalid_argument("Trying to add a null pointer to the table!");
-        }
+    void const * const                      toBeAdded(reinterpret_cast<const void* const>(templatePointer));
+    size_t                                  empty_index(0);
 
+    {
+        LockGuard const                     lock(m_un_mutex);
 
-        const void* const toBeAdded = reinterpret_cast<const void* const>(templatePointer);
-        
-        // since we are controling where and how to use PointerTable and Add function
+        std::ignore = lock;
+
+        // since we are controlling where and how to use PointerTable and Add function
         // it wouldn't worth the cost to have two maps with atomic insertions
         // check for duplicates at run time
         // comparing to the chance of adding the same pointer twice,
         // so we would only check for duplicates in debug mode
-        #ifndef NDEBUG
+#if (defined DEBUG)
             for (auto pairs : m_un) {
                 assert(pairs.second != toBeAdded);
             }
-        #endif
-
-        size_t empty_index = 0;
+#endif
 
         // size would be changing throughout the process
         // index zero is reserved
-        std::uniform_int_distribution<size_t> dist(1,std::numeric_limits<std::size_t>::max());
+        std::uniform_int_distribution<size_t>           dist(1,std::numeric_limits<std::size_t>::max());
 
         while(true) {
             size_t rand_index = dist(m_mt);
@@ -96,37 +108,48 @@ class PointerTable {
         }
         assert(empty_index != 0);
         m_un[empty_index] = toBeAdded;
-        return empty_index;
-    }
-    
-    template<typename T>
-    T* PointerTable::Get(size_t index) {
-        // pre-check
-        if (index == 0) {
-            throw std::invalid_argument("Invalid query to the Pointer table, index cannot be zero!");
-        }
-
-
-        std::unordered_map<std::size_t, const void*>::const_iterator found = m_un.find(index);
-        if (found == m_un.end()) {
-            throw std::invalid_argument("Invalid query to the Pointer table, index incorrect!");
-        }
-        return reinterpret_cast<T*>(const_cast<void*>(found->second));
     }
 
+    return empty_index;
+}
 
-    inline void PointerTable::Remove(size_t index) {
-        // pre-check
-        if (index == 0) {
-            throw std::invalid_argument("Invalid remove from the Pointer table, index cannot be zero!");
-        }
-
-
-        std::unordered_map<std::size_t, const void*>::const_iterator found = m_un.find(index);
-        if (found == m_un.end()) {
-            throw std::invalid_argument("Invalid remove from the Pointer table, index pointer not found!");
-        }
-        m_un.erase(found);
+template<typename T>
+T* PointerTable::Get(size_t index) {
+    // pre-check
+    if (index == 0) {
+        throw std::invalid_argument("Invalid query to the Pointer table, index cannot be zero!");
     }
+
+    LockGuard const                         lock(m_un_mutex);
+
+    std::ignore = lock;
+
+    std::unordered_map<std::size_t, const void*>::const_iterator found = m_un.find(index);
+    if (found == m_un.end()) {
+        throw std::invalid_argument("Invalid query to the Pointer table, index incorrect!");
+    }
+
+    return reinterpret_cast<T*>(const_cast<void*>(found->second));
+}
+
+
+inline void PointerTable::Remove(size_t index) {
+    // pre-check
+    if (index == 0) {
+        throw std::invalid_argument("Invalid remove from the Pointer table, index cannot be zero!");
+    }
+
+    LockGuard const                         lock(m_un_mutex);
+
+    std::ignore = lock;
+
+    std::unordered_map<std::size_t, const void*>::const_iterator found = m_un.find(index);
+    if (found == m_un.end()) {
+        throw std::invalid_argument("Invalid remove from the Pointer table, index pointer not found!");
+    }
+
+    m_un.erase(found);
+}
+
 } // namespace Featurizer
 } // namespace Microsoft
