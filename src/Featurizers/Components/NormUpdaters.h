6b78170f25fd9d276ab284d326a72b536b96c161
Jin Yan <jiyan2@microsoft.com>
2019-12-12 11:34:18 -0800

    standard deviation and norm updater (#26)
    * rebase on master
    
    * fix double and long double
    
    * fix fuzzy check
    
    * fix comments
    
    * fix comments
    
    * change namespace to Updaters
    
    * fix name in comment

---
+++ src/Featurizers/Components/NormUpdaters.h
@@ -0,0 +1,302 @@
+// ----------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License
+// ----------------------------------------------------------------------
+#pragma once
+
+#include <limits>
+#include "../../Traits.h"
+namespace Microsoft {
+namespace Featurizer {
+namespace Featurizers {
+namespace Components {
+
+namespace TypeSelector {
+// max norm type selector is created for selecting different types for max norm based on input type
+// when input type is integer, uint64 is chosen for performance
+// when input type is numeric, long double is chosen
+template <typename T>
+struct MaxNormTypeSelector {
+    using type = std::uint64_t;
+};
+template <>
+struct MaxNormTypeSelector<std::float_t> {
+    using type = long double;
+};
+
+template <>
+struct MaxNormTypeSelector<std::double_t> {
+    using type = long double;
+};
+
+}
+
+#if (defined __clang__)
+#   pragma clang diagnostic push
+#   pragma clang diagnostic ignored "-Wfloat-equal"
+#   pragma clang diagnostic ignored "-Wdouble-promotion"
+#endif
+
+namespace Updaters{
+
+/////////////////////////////////////////////////////////////////////////
+///  \class         L1NormUpdater
+///  \brief         L1NormUpdater updates l1 norm
+///                 l1 norm is the sum of elements absolute value
+///
+template <typename T>
+class L1NormUpdater {
+public:
+
+    // ----------------------------------------------------------------------
+    // |
+    // |  Public Types
+    // |
+    // ----------------------------------------------------------------------
+    using InputType                         = T;
+
+    // input type can only be integer or numeric
+    static_assert(Traits<T>::IsIntOrNumeric::value, "Input type to norm updater has to be integer or numerical types!");
+
+    // ----------------------------------------------------------------------
+    // |
+    // |  Public Methods
+    // |
+    // ----------------------------------------------------------------------
+
+    L1NormUpdater(void);
+
+    void update(InputType input);
+
+    long double commit(void);
+private:
+    // ----------------------------------------------------------------------
+    // |
+    // |  Private Data
+    // |
+    // ----------------------------------------------------------------------
+    long double _l1_norm;
+
+    // flags to check if update function is called
+    // an error will be thrown if no update(input) is called before commit
+    // since it doesn't make sense to create an updater and commit without any update
+    bool        _update_flag;
+};
+
+
+/////////////////////////////////////////////////////////////////////////
+///  \class         L2NormUpdater
+///  \brief         L2NormUpdater updates l2 norm
+///                 l2 norm is the sum of square of elements absolute value
+///
+template <typename T>
+class L2NormUpdater {
+public:
+
+    // ----------------------------------------------------------------------
+    // |
+    // |  Public Types
+    // |
+    // ----------------------------------------------------------------------
+    using InputType                         = T;
+
+    // input type can only be integer or numeric
+    static_assert(Traits<T>::IsIntOrNumeric::value, "Input type to norm updater has to be integer or numerical types!");
+
+    // ----------------------------------------------------------------------
+    // |
+    // |  Public Methods
+    // |
+    // ----------------------------------------------------------------------
+
+    L2NormUpdater(void);
+
+    void update(InputType input);
+
+    long double commit(void);
+private:
+    // ----------------------------------------------------------------------
+    // |
+    // |  Private Data
+    // |
+    // ----------------------------------------------------------------------
+    long double _l2_norm;
+
+    // flags to check if update functions are called
+    // an error will be thrown if no update(input) is called before commit
+    // since it doesn't make sense to create an updater and commit without any update
+    bool        _update_flag;
+};
+
+
+/////////////////////////////////////////////////////////////////////////
+///  \class         MaxNormUpdater
+///  \brief         MaxNormUpdater updates max norm
+///                 max norm is the maximum absolute value of input data elements
+///
+template <typename T>
+class MaxNormUpdater {
+public:
+
+    // ----------------------------------------------------------------------
+    // |
+    // |  Public Types
+    // |
+    // ----------------------------------------------------------------------
+    using InputType                         = T;
+
+    // input type can only be integer or numeric
+    static_assert(Traits<T>::IsIntOrNumeric::value, "Input type to norm updater has to be integer or numerical types!");
+
+    // ----------------------------------------------------------------------
+    // |
+    // |  Public Methods
+    // |
+    // ----------------------------------------------------------------------
+
+    MaxNormUpdater(void);
+
+    void update(InputType input);
+
+    typename TypeSelector::MaxNormTypeSelector<T>::type commit(void);
+private:
+    // ----------------------------------------------------------------------
+    // |
+    // |  Private Data
+    // |
+    // ----------------------------------------------------------------------
+    typename TypeSelector::MaxNormTypeSelector<T>::type _max_norm;
+
+    // flags to check if update functions are called
+    // an error will be thrown if no update(input) is called before commit
+    // since it doesn't make sense to create an updater and commit without any update
+    bool                                                _update_flag;
+};
+} // Updaters
+
+// ----------------------------------------------------------------------
+// ----------------------------------------------------------------------
+// ----------------------------------------------------------------------
+// |
+// |  Implementation
+// |
+// ----------------------------------------------------------------------
+// ----------------------------------------------------------------------
+// ----------------------------------------------------------------------
+
+// ----------------------------------------------------------------------
+// |
+// |  Updaters::L1NormUpdater
+// |
+// ----------------------------------------------------------------------
+template <typename T>
+Updaters::L1NormUpdater<T>::L1NormUpdater() :
+    _l1_norm(0),
+    _update_flag(false) {
+}
+
+template <typename T>
+void Updaters::L1NormUpdater<T>::update(T input) {
+    if (!_update_flag) {
+        _update_flag = true;
+    }
+    long double diff = std::abs(static_cast<long double>(input));
+
+    // check if diff is too small comparing to l1_norm
+    if ((_l1_norm + diff == _l1_norm) && (diff != 0)) {
+        throw std::runtime_error("In l1 norm calculation, input is so small comparing to l1_norm that l1_norm is the same after long double addition!");
+    }
+    _l1_norm += diff;
+
+    // check if exceeds bound
+    // only check for l2_norm and no checks for l1_norm is because if l1_norm is out of upper bound, l2_norm is definitely out of bound
+    if (std::isinf(_l1_norm)) {
+        throw std::runtime_error("Overflows occured during calculating l1_norm! Check your data!");
+    }
+}
+
+template <typename T>
+long double Updaters::L1NormUpdater<T>::commit(void) {
+    assert(_l1_norm >= 0);
+    if (!_update_flag) {
+        throw std::runtime_error("update is not called before l1 is committed!");
+    }
+    return _l1_norm;
+}
+// ----------------------------------------------------------------------
+// |
+// |  Updaters::L2NormUpdater
+// |
+// ----------------------------------------------------------------------
+template <typename T>
+Updaters::L2NormUpdater<T>::L2NormUpdater() :
+    _l2_norm(0),
+    _update_flag(false) {
+}
+
+template <typename T>
+void Updaters::L2NormUpdater<T>::update(T input) {
+    if (!_update_flag) {
+        _update_flag = true;
+    }
+    long double diff_square = std::pow(std::abs(static_cast<long double>(input)), 2);
+
+    if ((_l2_norm + diff_square == _l2_norm) && (diff_square != 0)) {
+        throw std::runtime_error("In l2 norm calculation, square of input is so small comparing to l2_norm that l2_norm is the same after long double addition!");
+    }
+    _l2_norm += diff_square;
+
+    // check if exceeds bound
+    // only check for l2_norm and no checks for l1_norm is because if l1_norm is out of upper bound, l2_norm is definitely out of bound
+    if (std::isinf(_l2_norm)) {
+        throw std::runtime_error("Overflows occured during calculating l2_norm! Check your data!");
+    }
+}
+
+template <typename T>
+long double Updaters::L2NormUpdater<T>::commit(void) {
+    assert(_l2_norm >= 0);
+    if (!_update_flag) {
+        throw std::runtime_error("update is not called before l2 is committed!");
+    }
+    return std::sqrt(_l2_norm);
+}
+// ----------------------------------------------------------------------
+// |
+// |  Updaters::MaxNormUpdater
+// |
+// ----------------------------------------------------------------------
+template <typename T>
+Updaters::MaxNormUpdater<T>::MaxNormUpdater() :
+    _max_norm(0),
+    _update_flag(false) {
+}
+
+template <typename T>
+void Updaters::MaxNormUpdater<T>::update(T input) {
+    if (!_update_flag) {
+        _max_norm = static_cast<typename TypeSelector::MaxNormTypeSelector<T>::type>(std::abs(input));
+        _update_flag = true;
+    } else {
+        _max_norm = std::max(static_cast<typename TypeSelector::MaxNormTypeSelector<T>::type>(std::abs(input)), _max_norm);
+    }
+}
+
+
+template <typename T>
+typename TypeSelector::MaxNormTypeSelector<T>::type Updaters::MaxNormUpdater<T>::commit(void) {
+    assert(_max_norm >= 0);
+    if (!_update_flag) {
+        throw std::runtime_error("update is not called before max is committed!");
+    }
+    return _max_norm;
+}
+
+#if (defined __clang__)
+#   pragma clang diagnostic pop
+#endif
+
+}
+}
+}
+}
---
+++ src/Featurizers/Components/UnitTests/NormUpdaters_UnitTest.cpp
@@ -0,0 +1,84 @@
+// ----------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License
+// ----------------------------------------------------------------------
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+
+#include "../../../3rdParty/optional.h"
+#include "../../TestHelpers.h"
+#include "../NormUpdaters.h"
+namespace NS = Microsoft::Featurizer;
+
+#if (defined __clang__)
+#   pragma clang diagnostic push
+#   pragma clang diagnostic ignored "-Wfloat-equal"
+#   pragma clang diagnostic ignored "-Wdouble-promotion"
+#endif
+
+// updater tests
+
+TEST_CASE("l1 updater normal cases") {
+    using inputType = int;
+    NS::Featurizers::Components::Updaters::L1NormUpdater<inputType> updater;
+    CHECK_THROWS_WITH(updater.commit(), "update is not called before l1 is committed!");
+
+    updater.update(0);
+    long double const r1(updater.commit());
+    CHECK(r1 == 0);
+
+    updater.update(1);
+    updater.update(2);
+    updater.update(3);
+    long double const r2(updater.commit());
+    CHECK(r2 == 6);
+}
+TEST_CASE("l1 updater overflow") {
+    using inputType = std::double_t;
+    NS::Featurizers::Components::Updaters::L1NormUpdater<inputType> updater;
+    updater.update(std::numeric_limits<inputType>::max());
+    CHECK_THROWS_WITH(updater.update(1), "In l1 norm calculation, input is so small comparing to l1_norm that l1_norm is the same after long double addition!");
+}
+
+TEST_CASE("l2 updater normal cases") {
+    using inputType = int;
+    NS::Featurizers::Components::Updaters::L2NormUpdater<inputType> updater;
+    CHECK_THROWS_WITH(updater.commit(), "update is not called before l2 is committed!");
+
+    updater.update(2);
+    long double const r1(updater.commit());
+    CHECK(r1 == 2);
+
+    updater.update(1);
+    updater.update(2);
+    updater.update(4);
+    long double const r2(updater.commit());
+    CHECK(r2 == 5);
+}
+TEST_CASE("l2 updater overflow") {
+    using inputType = std::double_t;
+    NS::Featurizers::Components::Updaters::L2NormUpdater<inputType> updater;
+    updater.update(std::sqrt(std::numeric_limits<inputType>::max()));
+    CHECK_THROWS_WITH(updater.update(1), "In l2 norm calculation, square of input is so small comparing to l2_norm that l2_norm is the same after long double addition!");
+}
+
+TEST_CASE("max updater") {
+    using inputType = int;
+    NS::Featurizers::Components::Updaters::MaxNormUpdater<inputType> updater;
+    CHECK_THROWS_WITH(updater.commit(), "update is not called before max is committed!");
+
+    updater.update(-10);
+    std::uint64_t const r1(updater.commit());
+    CHECK(r1 == 10);
+
+    updater.update(-10);
+    updater.update(1);
+    updater.update(100);
+    std::uint64_t const r2(updater.commit());
+    CHECK(r2 == 100);
+
+}
+
+#if (defined __clang__)
+#   pragma clang diagnostic pop
+#endif
