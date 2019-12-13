97766db57d74453460d7471e03049150f6d59425
Jin Yan <jiyan2@microsoft.com>
2019-12-12 16:38:54 -0800

    standard scaler in process

---
+++ src/Featurizers/Components/StandardDeviationEstimator.h
@@ -0,0 +1,261 @@
+// ----------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License
+// ----------------------------------------------------------------------
+#pragma once
+
+#include <limits>
+
+#include "TrainingOnlyEstimatorImpl.h"
+#include "../../Traits.h"
+#include "NormUpdaters.h"
+#include "StatisticsMetricsEstimator.h"
+namespace Microsoft {
+namespace Featurizer {
+namespace Featurizers {
+namespace Components {
+
+#if (defined __clang__)
+#   pragma clang diagnostic push
+#   pragma clang diagnostic ignored "-Wfloat-equal"
+#   pragma clang diagnostic ignored "-Wdouble-promotion"
+#endif
+
+static constexpr char const * const         StandardDeviationEstimatorName("StandardDeviationEstimator");
+
+/////////////////////////////////////////////////////////////////////////
+///  \class         StandardDeviationAnnotationData
+///  \brief         An annotation class which contains the standard deviation and count of input elements
+///                 for an input column
+///
+class StandardDeviationAnnotationData {
+public:
+    // ----------------------------------------------------------------------
+    // |
+    // |  Public Data
+    // |
+    // ----------------------------------------------------------------------
+    long double   const StandardDeviation;
+    std::uint64_t const Count;
+
+    // ----------------------------------------------------------------------
+    // |
+    // |  Public Methods
+    // |
+    // ----------------------------------------------------------------------
+    StandardDeviationAnnotationData(long double standard_deviation, std::uint64_t count);
+    ~StandardDeviationAnnotationData(void) = default;
+
+    FEATURIZER_MOVE_CONSTRUCTOR_ONLY(StandardDeviationAnnotationData);
+};
+
+namespace Details {
+
+/////////////////////////////////////////////////////////////////////////
+///  \class         StandardDeviationTrainingOnlyPolicy
+///  \brief         `StandardDeviationTrainingOnlyPolicy` implementation details.
+///
+template <typename T, typename EstimatorT>
+class StandardDeviationTrainingOnlyPolicy {
+public:
+    // ----------------------------------------------------------------------
+    // |
+    // |  Public Types
+    // |
+    // ----------------------------------------------------------------------
+    using InputType                         = T;
+
+    // ----------------------------------------------------------------------
+    // |
+    // |  Public Data
+    // |
+    // ----------------------------------------------------------------------
+    static constexpr char const * const     NameValue = StandardDeviationEstimatorName;
+
+    // ----------------------------------------------------------------------
+    // |
+    // |  Public Methods
+    // |
+    // ----------------------------------------------------------------------
+    // this constructor is used for StandardDeviationEstimator following StatisticsMetricsEstimator
+    StandardDeviationTrainingOnlyPolicy(void);
+    // this constructor is used for user inputs a specific average
+    StandardDeviationTrainingOnlyPolicy(std::double_t average);
+
+    bool begin_training(AnnotationMap const &annotations);
+    void fit(InputType const &input);
+    StandardDeviationAnnotationData complete_training(void);
+
+private:
+    // ----------------------------------------------------------------------
+    // |
+    // |  Private Types
+    // |
+    // ----------------------------------------------------------------------
+    Updaters::L2NormUpdater<std::double_t>    _updater;
+    std::double_t                             _average;
+    std::uint64_t                             _count;
+    // this flag is to mark if user input a specific average
+    bool                                      _user_average_flag;
+};
+
+} // namespace Details
+
+
+
+/////////////////////////////////////////////////////////////////////////
+///  \typedef       StandardDeviationEstimator
+///  \brief         A training-only class that calculate standard deviation based on
+///                 average and count
+///
+template <
+    typename T,
+    size_t MaxNumTrainingItemsV=std::numeric_limits<size_t>::max()
+>
+class StandardDeviationEstimator :
+    public TrainingOnlyEstimatorImpl<
+        Details::StandardDeviationTrainingOnlyPolicy<
+            T,
+            StandardDeviationEstimator<T, MaxNumTrainingItemsV>
+        >,
+        MaxNumTrainingItemsV
+    > {
+public:
+    // ----------------------------------------------------------------------
+    // |
+    // |  Public Types
+    // |
+    // ----------------------------------------------------------------------
+    using BaseType =
+        TrainingOnlyEstimatorImpl<
+            Details::StandardDeviationTrainingOnlyPolicy<
+                T,
+                StandardDeviationEstimator<T, MaxNumTrainingItemsV>
+            >,
+            MaxNumTrainingItemsV
+        >;
+
+    // ----------------------------------------------------------------------
+    // |
+    // |  Public Methods
+    // |
+    // ----------------------------------------------------------------------
+    StandardDeviationEstimator(AnnotationMapsPtr pAllColumnAnnotations, size_t colIndex);
+    StandardDeviationEstimator(AnnotationMapsPtr pAllColumnAnnotations, size_t colIndex, std::double_t average);
+
+    ~StandardDeviationEstimator(void) override = default;
+
+    FEATURIZER_MOVE_CONSTRUCTOR_ONLY(StandardDeviationEstimator);
+};
+
+
+// ----------------------------------------------------------------------
+// ----------------------------------------------------------------------
+// ----------------------------------------------------------------------
+// |
+// |  Implementation
+// |
+// ----------------------------------------------------------------------
+// ----------------------------------------------------------------------
+// ----------------------------------------------------------------------
+
+// ----------------------------------------------------------------------
+// |
+// |  StandardDeviationAnnotationData
+// |
+// ----------------------------------------------------------------------
+
+StandardDeviationAnnotationData::StandardDeviationAnnotationData(long double standard_deviation, std::uint64_t count) :
+    StandardDeviation(std::move(standard_deviation)),
+    Count(std::move(count)) {
+        if (StandardDeviation < 0) {
+            throw std::invalid_argument("Standard deviation shouldn't be less than 0!");
+        }
+        if ((Count == 0) && (StandardDeviation != 0)) {
+            throw std::invalid_argument("Standard deviation is not 0 when count is 0!");
+        }
+}
+
+// ----------------------------------------------------------------------
+// |
+// |  StandardDeviationEstimator
+// |
+// ----------------------------------------------------------------------
+template <typename T, size_t MaxNumTrainingItemsV>
+StandardDeviationEstimator<T, MaxNumTrainingItemsV>::StandardDeviationEstimator(AnnotationMapsPtr pAllColumnAnnotations, size_t colIndex) :
+    BaseType(std::move(pAllColumnAnnotations), std::move(colIndex)) {
+}
+
+template <typename T, size_t MaxNumTrainingItemsV>
+StandardDeviationEstimator<T, MaxNumTrainingItemsV>::StandardDeviationEstimator(AnnotationMapsPtr pAllColumnAnnotations, size_t colIndex, std::double_t average) :
+    BaseType(std::move(pAllColumnAnnotations), std::move(colIndex), true, std::move(average)) {
+}
+
+// ----------------------------------------------------------------------
+// |
+// |  Details::StandardDeviationTrainingOnlyPolicy
+// |
+// ----------------------------------------------------------------------
+template <typename T, typename StandardDeviationEstimatorT>
+Details::StandardDeviationTrainingOnlyPolicy<T, StandardDeviationEstimatorT>::StandardDeviationTrainingOnlyPolicy(void) :
+    _updater(Updaters::L2NormUpdater<std::double_t>()),
+    _average(0),
+    _count(0),
+    _user_average_flag(false) {
+}
+
+template <typename T, typename StandardDeviationEstimatorT>
+Details::StandardDeviationTrainingOnlyPolicy<T, StandardDeviationEstimatorT>::StandardDeviationTrainingOnlyPolicy(std::double_t average) :
+    _updater(Updaters::L2NormUpdater<std::double_t>()),
+    _average(std::move(average)),
+    _count(0),
+    _user_average_flag(true) {
+}
+
+template <typename T, typename StandardDeviationEstimatorT>
+bool Details::StandardDeviationTrainingOnlyPolicy<T, StandardDeviationEstimatorT>::begin_training(AnnotationMap const &annotations) {
+    // if user doesn't specify an average value, retrieve the StatisticalMetricsEstimator annotation to get average and count
+    if (!_user_average_flag) {
+        // ----------------------------------------------------------------------
+        using StandardStatisticalAnnotationData            = Components::StandardStatisticalAnnotationData<T>;
+        using StatisticalMetricsEstimator                  = Components::StatisticalMetricsEstimator<T, StandardDeviationEstimatorT::MaxNumTrainingItems>;
+        // ----------------------------------------------------------------------
+
+        StandardDeviationEstimatorT       const &          estimator(static_cast<StandardDeviationEstimatorT const &>(*this));
+        StandardStatisticalAnnotationData const &          data(StatisticalMetricsEstimator::get_annotation_data(estimator.get_column_annotations(), estimator.get_column_index(), StatisticalMetricsEstimatorName));
+
+        _average = data.Average;
+        _count = data.Count;
+    }
+    return true;
+}
+
+template <typename T, typename StandardDeviationEstimatorT>
+void Details::StandardDeviationTrainingOnlyPolicy<T, StandardDeviationEstimatorT>::fit(InputType const &input) {
+    if(Microsoft::Featurizer::Traits<T>::IsNull(input))
+        return;
+    _updater.update(static_cast<std::double_t>(input) - _average);
+    // check if count will be out of bounds
+    if (std::numeric_limits<std::uint64_t>::max() == _count) {
+        throw std::runtime_error("Overflow occured for count during calculating standard deviation! You might input too much data");
+    }
+    ++_count;
+}
+
+template <typename T, typename StandardDeviationEstimatorT>
+StandardDeviationAnnotationData Details::StandardDeviationTrainingOnlyPolicy<T, StandardDeviationEstimatorT>::complete_training(void) {
+    long double deviation = _updater.commit()/static_cast<long double>(_count);
+    assert(deviation >= 0);
+    return StandardDeviationAnnotationData(std::move(deviation), std::move(_count));
+}
+
+
+#if (defined __clang__)
+#   pragma clang diagnostic pop
+#endif
+
+
+}
+}
+}
+}
---
+++ src/Featurizers/Components/UnitTests/StandardDeviationEstimator_UnitTest.cpp
@@ -0,0 +1,97 @@
+// ----------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License
+// ----------------------------------------------------------------------
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+
+#include "../../../3rdParty/optional.h"
+#include "../../TestHelpers.h"
+#include "../StandardDeviationEstimator.h"
+namespace NS = Microsoft::Featurizer;
+
+#if (defined __clang__)
+#   pragma clang diagnostic push
+#   pragma clang diagnostic ignored "-Wfloat-equal"
+#   pragma clang diagnostic ignored "-Wdouble-promotion"
+#endif
+
+// test for annotation data
+
+TEST_CASE("valid argument for annotation data") {
+    NS::Featurizers::Components::StandardDeviationAnnotationData deviation(1,3);
+    CHECK(deviation.StandardDeviation == 1);
+    CHECK(deviation.Count == 3);
+}
+
+TEST_CASE("invalid argument for annotation data") {
+    CHECK_THROWS_WITH(NS::Featurizers::Components::StandardDeviationAnnotationData(-1, 1), "Standard deviation shouldn't be less than 0!");
+    CHECK_THROWS_WITH(NS::Featurizers::Components::StandardDeviationAnnotationData(3, 0), "Standard deviation is not 0 when count is 0!");
+}
+
+
+
+// test for overall estimator
+TEST_CASE("one int input data") {
+    using inputType = int;
+
+    NS::AnnotationMapsPtr pAllColumnAnnotations(NS::CreateTestAnnotationMapsPtr(1));
+    NS::Featurizers::Components::StandardDeviationEstimator<inputType> estimator(pAllColumnAnnotations, 0,
+                                                                                 10                         // user defined average
+                                                                                 );
+
+    NS::TestHelpers::Train(estimator, std::vector<std::vector<inputType>>({{10}}));
+    NS::Featurizers::Components::StandardDeviationAnnotationData const& stats(estimator.get_annotation_data());
+
+    CHECK(stats.StandardDeviation == 0);
+    CHECK(stats.Count             == 1);
+}
+
+TEST_CASE("N random double input data") {
+    using inputType = std::double_t;
+
+    NS::AnnotationMapsPtr pAllColumnAnnotations(NS::CreateTestAnnotationMapsPtr(1));
+    NS::Featurizers::Components::StandardDeviationEstimator<inputType> estimator(pAllColumnAnnotations, 0,
+                                                                                 16.7                         // user defined average
+                                                                                 );
+
+    NS::TestHelpers::Train(estimator, std::vector<std::vector<inputType>>({{
+                                                                            10.3l,
+                                                                            20.1l,
+                                                                            8.4l,
+                                                                            8.2l,
+                                                                            10.3l,
+                                                                            30.1l,
+                                                                            30.4l,
+                                                                            15.8l}}));
+    NS::Featurizers::Components::StandardDeviationAnnotationData const& stats(estimator.get_annotation_data());
+
+    CHECK(NS::TestHelpers::FuzzyCheck<long double>({stats.StandardDeviation}, {static_cast<long double>(3.068693)}));
+    CHECK(stats.Count == 8);
+}
+
+
+TEST_CASE("all same input") {
+    using inputType = int;
+
+    NS::AnnotationMapsPtr pAllColumnAnnotations(NS::CreateTestAnnotationMapsPtr(1));
+    NS::Featurizers::Components::StandardDeviationEstimator<inputType> estimator(pAllColumnAnnotations, 0,
+                                                                                 15                         // user defined average
+                                                                                 );
+
+    NS::TestHelpers::Train(estimator, std::vector<std::vector<inputType>>({{15,
+                                                                            15,
+                                                                            15,
+                                                                            15,
+                                                                            15,
+                                                                            15,
+                                                                            15}}));
+    NS::Featurizers::Components::StandardDeviationAnnotationData const& stats(estimator.get_annotation_data());
+
+    CHECK(stats.StandardDeviation == 0);
+    CHECK(stats.Count == 7);
+}
+
+#if (defined __clang__)
+#   pragma clang diagnostic pop
+#endif
