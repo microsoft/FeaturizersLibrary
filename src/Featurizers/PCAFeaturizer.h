3f87a382121479ec8370ec5b353aa51b3816dd55
wangyems <wangye@microsoft.com>
2020-01-15 16:13:24 -0800

    refactor PCA featurizer

---
+++ src/Featurizers/PCAFeaturizer.h
@@ -0,0 +1,258 @@
+// ----------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License
+// ----------------------------------------------------------------------
+#pragma once
+
+#include "../Featurizer.h"
+#include "../Archive.h"
+
+namespace Microsoft {
+namespace Featurizer {
+namespace Featurizers {
+
+/////////////////////////////////////////////////////////////////////////
+///  \class         PCATransformer
+///  \brief         Contains PCAComponents and use PCAComponents to project
+///                 matrix for dimensionality reduction, also provides
+///                 PCAComponents retriving
+///
+template <typename MatrixT>
+class PCATransformer : public StandardTransformer<MatrixT, MatrixT> {
+public:
+    // ----------------------------------------------------------------------
+    // |
+    // |  Public Types
+    // |
+    // ----------------------------------------------------------------------
+    using BaseType                                   = StandardTransformer<MatrixT, MatrixT>;
+
+    // ----------------------------------------------------------------------
+    // |
+    // |  Public Methods
+    // |
+    // ----------------------------------------------------------------------
+    PCATransformer(MatrixT eigenvalues, MatrixT eigenvectors);
+    PCATransformer(Archive &ar);
+
+    ~PCATransformer(void) override = default;
+
+    FEATURIZER_MOVE_CONSTRUCTOR_ONLY(PCATransformer);
+
+    bool operator==(PCATransformer const &other) const;
+
+    void save(Archive &ar) const override;
+
+private:
+    // ----------------------------------------------------------------------
+    // |
+    // |  Private Data
+    // |
+    // ----------------------------------------------------------------------
+    MatrixT const                                    _eigenvalues;
+    MatrixT const                                    _eigenvectors;
+
+    // ----------------------------------------------------------------------
+    // |
+    // |  Private Methods
+    // |
+    // ----------------------------------------------------------------------
+     // MSVC has problems when the declaration and definition are separated
+    void execute_impl(typename BaseType::InputType const &input, typename BaseType::CallbackFunction const &callback) override {
+        if (input.size() == 0)
+            throw std::invalid_argument("Input matrix size() invalid");
+
+        if (input.cols() != _eigenvectors.cols())
+            throw std::invalid_argument("Input matrix cols() invalid");
+
+        callback(input * _eigenvectors.transpose());
+    }
+};
+
+/////////////////////////////////////////////////////////////////////////
+///  \class         PCAEstimator
+///  \brief         Estimator to calculate PCAComponents
+///
+template <
+    typename MatrixT,
+    size_t MaxNumTrainingItemsV=std::numeric_limits<size_t>::max()
+>
+class PCAEstimator : public TransformerEstimator<MatrixT, MatrixT> {
+public:
+    // ----------------------------------------------------------------------
+    // |
+    // |  Public Types
+    // |
+    // ----------------------------------------------------------------------
+    using BaseType                          = TransformerEstimator<MatrixT, MatrixT>;
+    using TransformerType                   = PCATransformer<MatrixT>;
+
+    // ----------------------------------------------------------------------
+    // |
+    // |  Public Methods
+    // |
+    // ----------------------------------------------------------------------
+    PCAEstimator(AnnotationMapsPtr pAllColumnAnnotations, size_t colIndex);
+    ~PCAEstimator(void) override = default;
+
+    FEATURIZER_MOVE_CONSTRUCTOR_ONLY(PCAEstimator);
+
+private:
+    // ----------------------------------------------------------------------
+    // |
+    // |  Private Data
+    // |
+    // ----------------------------------------------------------------------
+    size_t const                            _colIndex;
+    MatrixT                                 _matrix;
+    MatrixT                                 _eigenvalues;
+    MatrixT                                 _eigenvectors;
+
+    // ----------------------------------------------------------------------
+    // |
+    // |  Private Methods
+    // |
+    // ----------------------------------------------------------------------
+    bool begin_training_impl(void) override;
+    // MSVC has problems when the declaration and definition are separated
+    FitResult fit_impl(typename BaseType::InputType const *pBuffer, size_t) override {
+        _matrix = *pBuffer;
+
+        return FitResult::Continue;
+    }
+    void complete_training_impl(void) override;
+
+    // MSVC has problems when the definition is separate from the declaration
+    typename BaseType::TransformerUniquePtr create_transformer_impl(void) override {
+        return typename BaseType::TransformerUniquePtr(new PCATransformer<MatrixT>(_eigenvalues, _eigenvectors));
+    }
+};
+
+// ----------------------------------------------------------------------
+// ----------------------------------------------------------------------
+// ----------------------------------------------------------------------
+// |
+// |  Implementation
+// |
+// ----------------------------------------------------------------------
+// ----------------------------------------------------------------------
+// ----------------------------------------------------------------------
+
+// ----------------------------------------------------------------------
+// |
+// |  PCATransformer
+// |
+// ----------------------------------------------------------------------
+template <typename MatrixT>
+PCATransformer<MatrixT>::PCATransformer(MatrixT eigenvalues, MatrixT eigenvectors) :
+    _eigenvalues(
+        std::move(
+            [&eigenvalues](void) -> MatrixT & {
+                if(eigenvalues.size() == 0)
+                    throw std::invalid_argument("eigenvalues");
+
+                return eigenvalues;
+            }()
+        )
+    ),
+    _eigenvectors(
+        std::move(
+            [&eigenvectors](void) -> MatrixT & {
+                if(eigenvectors.size() == 0)
+                    throw std::invalid_argument("eigenvectors");
+
+                return eigenvectors;
+            }()
+        )
+    ) {
+}
+
+template <typename MatrixT>
+PCATransformer<MatrixT>::PCATransformer(Archive &ar) :
+    PCATransformer(
+        [&ar](void) {
+            // Version
+            std::uint16_t                   majorVersion(Traits<std::uint16_t>::deserialize(ar));
+            std::uint16_t                   minorVersion(Traits<std::uint16_t>::deserialize(ar));
+
+            if(majorVersion != 1 || minorVersion != 0)
+                throw std::runtime_error("Unsupported archive version");
+
+            // Data
+            MatrixT                        eigenvalues(Traits<MatrixT>::deserialize(ar));
+            MatrixT                        eigenvectors(Traits<MatrixT>::deserialize(ar));
+
+            return PCATransformer<MatrixT>(std::move(eigenvalues), std::move(eigenvectors));
+        }()
+    ) {
+}
+
+template <typename MatrixT>
+bool PCATransformer<MatrixT>::operator==(PCATransformer const &other) const {
+    if ((this->_eigenvalues - other._eigenvalues).norm() > 0.000001f || (this->_eigenvectors - other._eigenvectors).norm() > 0.000001f)
+        return false;
+
+    return true;
+}
+
+template <typename MatrixT>
+void PCATransformer<MatrixT>::save(Archive &ar) const /*override*/ {
+    // Version
+    Traits<std::uint16_t>::serialize(ar, 1); // Major
+    Traits<std::uint16_t>::serialize(ar, 0); // Minor
+
+    // Data
+    Traits<decltype(_eigenvalues)>::serialize(ar, _eigenvalues);
+    Traits<decltype(_eigenvectors)>::serialize(ar, _eigenvectors);
+}
+
+// ----------------------------------------------------------------------
+// |
+// |  PCAEstimator
+// |
+// ----------------------------------------------------------------------
+template <typename MatrixT, size_t MaxNumTrainingItemsV>
+PCAEstimator<MatrixT, MaxNumTrainingItemsV>::PCAEstimator(AnnotationMapsPtr pAllColumnAnnotations, size_t colIndex) :
+    BaseType("SVDEstimatorImpl", std::move(pAllColumnAnnotations)),
+    _colIndex(
+        std::move(
+            [this, &colIndex](void) -> size_t & {
+                if(colIndex >= this->get_column_annotations().size())
+                    throw std::invalid_argument("colIndex");
+
+                return colIndex;
+            }()
+        )
+    ) {
+}
+
+// ----------------------------------------------------------------------
+// ----------------------------------------------------------------------
+// ----------------------------------------------------------------------
+template <typename MatrixT,size_t MaxNumTrainingItemsV>
+bool PCAEstimator<MatrixT, MaxNumTrainingItemsV>::begin_training_impl(void) /*override*/ {
+    return true;
+}
+
+template <typename MatrixT, size_t MaxNumTrainingItemsV>
+void PCAEstimator<MatrixT, MaxNumTrainingItemsV>::complete_training_impl(void) /*override*/ {
+
+    Eigen::SelfAdjointEigenSolver<MatrixT> eig(
+        [&]() -> MatrixT {
+            //compute for centered training data
+            MatrixT centered = _matrix.rowwise() - _matrix.colwise().mean();
+            //compute for covariance matrix
+            MatrixT cov = centered.adjoint() * centered;
+            //using covariance matrix to compute PCA
+            return cov;
+        }()
+    );
+
+    //PCA uses sigma but sklearn PCA uses sqrt(sigma), this implementation follows original principle.
+    this->_eigenvalues = eig.eigenvalues();
+    this->_eigenvectors = eig.eigenvectors();
+}
+
+} // namespace Featurizers
+} // namespace Featurizer
+} // namespace Microsoft
---
+++ src/Featurizers/UnitTests/PCAFeaturizer_UnitTests.cpp
@@ -0,0 +1,141 @@
+// ----------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License
+// ----------------------------------------------------------------------
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+
+#include "../../Featurizers/PCAFeaturizer.h"
+#include "../TestHelpers.h"
+#include "../../Archive.h"
+
+namespace NS = Microsoft::Featurizer;
+
+template <typename MatrixT>
+void PCATransformerTest() {
+    std::float_t eps = 0.0001f;
+
+    MatrixT trainingMatrix(6, 2);
+    trainingMatrix(0, 0) = -1;
+    trainingMatrix(0, 1) = -1;
+    trainingMatrix(1, 0) = -2;
+    trainingMatrix(1, 1) = -1;
+    trainingMatrix(2, 0) = -3;
+    trainingMatrix(2, 1) = -2;
+    trainingMatrix(3, 0) = 1;
+    trainingMatrix(3, 1) = 1;
+    trainingMatrix(4, 0) = 2;
+    trainingMatrix(4, 1) = 1;
+    trainingMatrix(5, 0) = 3;
+    trainingMatrix(5, 1) = 2;
+
+    MatrixT input = trainingMatrix;
+
+    #if (defined __clang__)
+    #   pragma clang diagnostic push
+    #   pragma clang diagnostic ignored "-Wdouble-promotion"
+    #endif
+
+    MatrixT label(6, 2);
+    label(0, 0) = -0.2935787f;
+    label(0, 1) = -1.3834058f;
+    label(1, 0) =  0.2513348f;
+    label(1, 1) = -2.2218980f;
+    label(2, 0) = -0.0422439f;
+    label(2, 1) = -3.6053038f;
+    label(3, 0) =  0.2935787f;
+    label(3, 1) =  1.3834058f;
+    label(4, 0) = -0.2513348f;
+    label(4, 1) =  2.2218980f;
+    label(5, 0) =  0.0422439f;
+    label(5, 1) =  3.6053038f;
+
+    #if (defined __clang__)
+    #   pragma clang diagnostic pop
+    #endif
+
+    using PCAEstimator                                 = NS::Featurizers::PCAEstimator<MatrixT>;
+    NS::AnnotationMapsPtr const                        pAllColumnAnnotations(NS::CreateTestAnnotationMapsPtr(1));
+
+    std::vector<std::vector<MatrixT>> trainingBatches  = NS::TestHelpers::make_vector<std::vector<MatrixT>>(
+        NS::TestHelpers::make_vector<MatrixT>(trainingMatrix)
+    );
+
+    std::vector<MatrixT> inputContainers = NS::TestHelpers::make_vector<MatrixT>(input);
+    std::vector<MatrixT> outputContainer = NS::TestHelpers::TransformerEstimatorTest(PCAEstimator(pAllColumnAnnotations, 0), trainingBatches, inputContainers);
+
+    MatrixT output = outputContainer[0];
+
+#if (defined __clang__)
+#   pragma clang diagnostic push
+#   pragma clang diagnostic ignored "-Wdouble-promotion"
+#endif
+
+    CHECK((output.cwiseProduct(output) - label.cwiseProduct(label)).norm() < eps);
+   
+#if (defined __clang__)
+#   pragma clang diagnostic pop
+#endif
+}
+
+TEST_CASE("Invalid_Annotation") {
+    using MatrixT = Eigen::MatrixX<float>;
+    MatrixT invalidEigenValues;
+    MatrixT invalidEigenVectors;
+    MatrixT validEigenValues(2, 2);
+    MatrixT validEigenVectors(2, 2);
+    CHECK_THROWS_WITH(NS::Featurizers::PCATransformer<MatrixT>(invalidEigenValues, validEigenVectors), "eigenvalues");
+    CHECK_THROWS_WITH(NS::Featurizers::PCATransformer<MatrixT>(validEigenValues, invalidEigenVectors), "eigenvectors");
+}
+
+TEST_CASE("PCATransformerTest") {
+    PCATransformerTest<Eigen::MatrixX<float>>();
+    PCATransformerTest<Eigen::MatrixX<double>>();
+}
+
+TEST_CASE("Serialization/Deserialization") {
+    using MatrixT = Eigen::MatrixX<float>;
+    using TransformerType = NS::Featurizers::PCATransformer<MatrixT>;
+
+    MatrixT eigenValues(3, 1);
+    eigenValues(0, 0) = 3.79535f;
+    eigenValues(1, 0) = 2.21387f;
+    eigenValues(2, 0) = 0.83309f;
+
+    MatrixT eigenVectors(3, 3);
+    eigenVectors(0, 0) =  0.805059f;
+    eigenVectors(0, 1) = -0.315908f;
+    eigenVectors(0, 2) = -0.502078f;
+    eigenVectors(1, 0) =  0.204052f;
+    eigenVectors(1, 1) =  0.942225f;
+    eigenVectors(1, 2) = -0.265660f;
+    eigenVectors(2, 0) =  0.556994f;
+    eigenVectors(2, 1) =  0.111422f;
+    eigenVectors(2, 2) =  0.823008f;
+    
+    TransformerType                         original(eigenValues, eigenVectors);
+
+    NS::Archive                             out;
+
+    original.save(out);
+
+    NS::Archive                             in(out.commit());
+    TransformerType                         other(in);
+
+    CHECK(other == original);
+}
+
+TEST_CASE("Serialization Version Error") {
+    NS::Archive                             out;
+
+    out.serialize(static_cast<std::uint16_t>(2));
+    out.serialize(static_cast<std::uint16_t>(0));
+
+    NS::Archive                             in(out.commit());
+
+    CHECK_THROWS_WITH(
+        (NS::Featurizers::PCATransformer<Eigen::MatrixX<float>>(in)),
+        Catch::Contains("Unsupported archive version")
+    );
+}
+
