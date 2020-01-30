44ea083c6b19c3e0e73afe7dd1eacf84ec8ba660
Jin Yan <jiyan2@microsoft.com>
2020-01-30 13:10:50 -0800

    add CountVectorizer shared library test

--- src/SharedLibrary/IntegrationTests/SharedLibrary_CountVecFeaturizer_IntegrationTest.cpp
+++ src/SharedLibrary/IntegrationTests/SharedLibrary_CountVecFeaturizer_IntegrationTest.cpp
@@ -7,4 +7,27 @@
 
 #include "GeneratedCode/SharedLibraryTests_CountVectorizerFeaturizer.h"
 
-// BugBug
+TEST_CASE("Standard") {
+    uint32_t ngram_max(3);
+    CountVectorizerFeaturizer_Test(
+        std::vector<std::string>{"oraNge apple oranGE grape", "grApe caRrOt carrot apple", "peach Banana orange banana"},
+        std::vector<std::string>{"banana grape grape apple apple apple orange"},
+        [](std::vector<Microsoft::Featurizer::Featurizers::SparseVectorEncoding<std::uint32_t>> const &args) {
+            REQUIRE(args.size() == 1);
+
+            Microsoft::Featurizer::Featurizers::SparseVectorEncoding<std::uint32_t> const & obj(args[0]);
+            return (obj.NumElements == 3 && obj.Values[0].Value == 3 && obj.Values[0].Index == 0
+                                         && obj.Values[1].Value == 2 && obj.Values[1].Index == 1
+                                         && obj.Values[2].Value == 1 && obj.Values[2].Index == 2);
+        },
+        true,
+        CountVectorizerFeaturizer_AnalyzerMethod_Word,
+        "",
+        1.0f,
+        0.0f,
+        &ngram_max,
+        static_cast<std::uint32_t>(1),
+        static_cast<std::uint32_t>(1),
+        false
+    );
+}
