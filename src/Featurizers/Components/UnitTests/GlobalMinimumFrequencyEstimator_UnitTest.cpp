2d190a4bad0274a52eac688be95220f8da798da8
Jin Yan <jiyan2@microsoft.com>
2020-03-05 15:55:15 -0800

    renamed to GlobalMinimumFrequencyEstimator

--- src/Featurizers/Components/UnitTests/GlobalMinimumFrequencyEstimator_UnitTest.cpp
+++ src/Featurizers/Components/UnitTests/GlobalMinimumFrequencyEstimator_UnitTest.cpp
@@ -6,7 +6,7 @@
 #include "catch.hpp"
 
 #include "../../TestHelpers.h"
-#include "../FrequencyMinimumEstimator.h"
+#include "../GlobalMinimumFrequencyEstimator.h"
 
 namespace NS = Microsoft::Featurizer;
 
@@ -19,45 +19,45 @@ std::chrono::system_clock::time_point AddMinutes(std::chrono::system_clock::time
 }
 
 
-TEST_CASE("FrequencyMinimumEstimator - one grain without gap") {
+TEST_CASE("GlobalMinimumFrequencyEstimator - one grain without gap") {
     std::chrono::system_clock::time_point now = std::chrono::system_clock::now();
 
     using inputType = std::tuple<std::string,std::chrono::system_clock::time_point>;
 
     NS::AnnotationMapsPtr pAllColumnAnnotations(NS::CreateTestAnnotationMapsPtr(1));
-    NS::Featurizers::Components::FrequencyMinimumEstimator<> estimator(pAllColumnAnnotations, 0);
+    NS::Featurizers::Components::GlobalMinimumFrequencyEstimator<> estimator(pAllColumnAnnotations, 0);
 
     NS::TestHelpers::Train(estimator, std::vector<std::vector<inputType>>({{std::make_tuple("one",AddDays(now, 0)),
                                                                             std::make_tuple("one",AddDays(now, 1)),
                                                                             std::make_tuple("one",AddDays(now, 2))}}));
-    NS::Featurizers::Components::FrequencyMinimumAnnotation const& frequency(estimator.get_annotation_data());
+    NS::Featurizers::Components::GlobalMinimumFrequencyAnnotation const& frequency(estimator.get_annotation_data());
 
     CHECK(frequency.Value == std::chrono::minutes(60 * 24));
 }
 
-TEST_CASE("FrequencyMinimumEstimator - one grain with gap") {
+TEST_CASE("GlobalMinimumFrequencyEstimator - one grain with gap") {
     std::chrono::system_clock::time_point now = std::chrono::system_clock::now();
 
     using inputType = std::tuple<std::string,std::chrono::system_clock::time_point>;
 
     NS::AnnotationMapsPtr pAllColumnAnnotations(NS::CreateTestAnnotationMapsPtr(1));
-    NS::Featurizers::Components::FrequencyMinimumEstimator<> estimator(pAllColumnAnnotations, 0);
+    NS::Featurizers::Components::GlobalMinimumFrequencyEstimator<> estimator(pAllColumnAnnotations, 0);
 
     NS::TestHelpers::Train(estimator, std::vector<std::vector<inputType>>({{std::make_tuple("one",AddDays(now, 0)),
                                                                             std::make_tuple("one",AddDays(now, 1)),
                                                                             std::make_tuple("one",AddDays(now, 3))}}));
-    NS::Featurizers::Components::FrequencyMinimumAnnotation const& frequency(estimator.get_annotation_data());
+    NS::Featurizers::Components::GlobalMinimumFrequencyAnnotation const& frequency(estimator.get_annotation_data());
 
     CHECK(frequency.Value == std::chrono::minutes(60 * 24));
 }
 
-TEST_CASE("FrequencyMinimumEstimator - two grains with different frequencies") {
+TEST_CASE("GlobalMinimumFrequencyEstimator - two grains with different frequencies") {
     std::chrono::system_clock::time_point now = std::chrono::system_clock::now();
 
     using inputType = std::tuple<std::string,std::chrono::system_clock::time_point>;
 
     NS::AnnotationMapsPtr pAllColumnAnnotations(NS::CreateTestAnnotationMapsPtr(1));
-    NS::Featurizers::Components::FrequencyMinimumEstimator<> estimator(pAllColumnAnnotations, 0);
+    NS::Featurizers::Components::GlobalMinimumFrequencyEstimator<> estimator(pAllColumnAnnotations, 0);
 
     NS::TestHelpers::Train(estimator, std::vector<std::vector<inputType>>({{std::make_tuple("one",AddDays(now, 0)),
                                                                             std::make_tuple("one",AddDays(now, 1)),
@@ -65,18 +65,18 @@ TEST_CASE("FrequencyMinimumEstimator - two grains with different frequencies") {
                                                                             std::make_tuple("two",AddDays(now, 0)),
                                                                             std::make_tuple("two",AddDays(now, 2)),
                                                                             std::make_tuple("two",AddDays(now, 4))}}));
-    NS::Featurizers::Components::FrequencyMinimumAnnotation const& frequency(estimator.get_annotation_data());
+    NS::Featurizers::Components::GlobalMinimumFrequencyAnnotation const& frequency(estimator.get_annotation_data());
 
     CHECK(frequency.Value == std::chrono::minutes(60 * 24));
 }
 
-TEST_CASE("FrequencyMinimumEstimator - two grains with mixed frequencies") {
+TEST_CASE("GlobalMinimumFrequencyEstimator - two grains with mixed frequencies") {
     std::chrono::system_clock::time_point now = std::chrono::system_clock::now();
 
     using inputType = std::tuple<std::string,std::chrono::system_clock::time_point>;
 
     NS::AnnotationMapsPtr pAllColumnAnnotations(NS::CreateTestAnnotationMapsPtr(1));
-    NS::Featurizers::Components::FrequencyMinimumEstimator<> estimator(pAllColumnAnnotations, 0);
+    NS::Featurizers::Components::GlobalMinimumFrequencyEstimator<> estimator(pAllColumnAnnotations, 0);
 
     NS::TestHelpers::Train(estimator, std::vector<std::vector<inputType>>({{std::make_tuple("one",AddMinutes(now, 0)),
                                                                             std::make_tuple("one",AddMinutes(now, 1)),
@@ -84,18 +84,18 @@ TEST_CASE("FrequencyMinimumEstimator - two grains with mixed frequencies") {
                                                                             std::make_tuple("two",AddDays(now, 0)),
                                                                             std::make_tuple("two",AddDays(now, 2)),
                                                                             std::make_tuple("two",AddDays(now, 4))}}));
-    NS::Featurizers::Components::FrequencyMinimumAnnotation const& frequency(estimator.get_annotation_data());
+    NS::Featurizers::Components::GlobalMinimumFrequencyAnnotation const& frequency(estimator.get_annotation_data());
 
     CHECK(frequency.Value == std::chrono::minutes(1));
 }
 
-TEST_CASE("FrequencyMinimumEstimator - Not in chronological order") {
+TEST_CASE("GlobalMinimumFrequencyEstimator - Not in chronological order") {
     std::chrono::system_clock::time_point now = std::chrono::system_clock::now();
 
     using inputType = std::tuple<std::string,std::chrono::system_clock::time_point>;
 
     NS::AnnotationMapsPtr pAllColumnAnnotations(NS::CreateTestAnnotationMapsPtr(1));
-    NS::Featurizers::Components::FrequencyMinimumEstimator<> estimator(pAllColumnAnnotations, 0);
+    NS::Featurizers::Components::GlobalMinimumFrequencyEstimator<> estimator(pAllColumnAnnotations, 0);
 
     CHECK_THROWS_WITH(NS::TestHelpers::Train(estimator, std::vector<std::vector<inputType>>({{std::make_tuple("one",AddMinutes(now, 0)),
                                                                                               std::make_tuple("one",AddDays(now, 1)),
