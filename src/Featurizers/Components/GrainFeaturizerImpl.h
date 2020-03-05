5b6d28a13ad0f5ac798629272a903fcb2556b231
David Brownell <david.brownell@microsoft.com>
2020-03-05 11:47:52 -0800

    Restored fully-qualified using statement

--- src/Featurizers/Components/GrainFeaturizerImpl.h
+++ src/Featurizers/Components/GrainFeaturizerImpl.h
@@ -190,7 +190,7 @@ public:
     using InputType                         = typename TheseGrainEstimatorTraits::InputType;
     using TransformedType                   = typename TheseGrainEstimatorTraits::TransformedType;
 
-    using GrainEstimatorAnnotation          = GrainEstimatorAnnotation<GrainType>;
+    using GrainEstimatorAnnotation          = Microsoft::Featurizer::Featurizers::Components::GrainEstimatorAnnotation<GrainType>;
     using CreateEstimatorFunc               = std::function<EstimatorT (AnnotationMapsPtr)>;
 
     // ----------------------------------------------------------------------
