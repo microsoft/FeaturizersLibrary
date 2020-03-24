f3577a77737fb0f3199696f059852f4982987ef1
Michael Sharp <misharp@microsoft.com>
2020-03-24 10:07:29 -0700

    fix build breaks

--- src/Featurizers/UnitTests/AnalyticalRollingWindowFeaturizer_UnitTest.cpp
+++ src/Featurizers/UnitTests/AnalyticalRollingWindowFeaturizer_UnitTest.cpp
@@ -16,76 +16,76 @@ TEST_CASE("Mean - int32, window size 1, horizon 1") {
     
     NS::Featurizers::AnalyticalRollingWindowTransformer<std::int32_t>                transformer(1, NS::Featurizers::AnalyticalRollingWindowCalculation::Mean, 1);
 
-    NS::TestHelpers::FuzzyCheck(transformer.execute(1), {std::nan("")});
+    CHECK(NS::TestHelpers::FuzzyCheck(transformer.execute(1), {std::nan("")}));
 
-    NS::TestHelpers::FuzzyCheck(transformer.execute(2), {1});
+    CHECK(NS::TestHelpers::FuzzyCheck(transformer.execute(2), {1}));
 
-    NS::TestHelpers::FuzzyCheck(transformer.execute(3), {2});
+    CHECK(NS::TestHelpers::FuzzyCheck(transformer.execute(3), {2}));
 
-    NS::TestHelpers::FuzzyCheck(transformer.execute(4), {3});
+    CHECK(NS::TestHelpers::FuzzyCheck(transformer.execute(4), {3}));
 }
 
 TEST_CASE("Mean - int32, window size 2, horizon 1") {    
     NS::Featurizers::AnalyticalRollingWindowTransformer<std::int32_t>                transformer(2, NS::Featurizers::AnalyticalRollingWindowCalculation::Mean, 1);
 
-    NS::TestHelpers::FuzzyCheck(transformer.execute(1), {std::nan("")});
+    CHECK(NS::TestHelpers::FuzzyCheck(transformer.execute(1), {std::nan("")}));
 
-    NS::TestHelpers::FuzzyCheck(transformer.execute(2), {1});
+    CHECK(NS::TestHelpers::FuzzyCheck(transformer.execute(2), {1}));
 
      // Correct result is now {1.5} because the mean of 1 + 2 is 1.5
-    NS::TestHelpers::FuzzyCheck(transformer.execute(3), {1.5});
+    CHECK(NS::TestHelpers::FuzzyCheck(transformer.execute(3), {1.5}));
 
     // Correct result is now {2.5} because the mean of 2 + 3 is 2.5
-    NS::TestHelpers::FuzzyCheck(transformer.execute(4), {2.5});
+    CHECK(NS::TestHelpers::FuzzyCheck(transformer.execute(4), {2.5}));
 }
 
 TEST_CASE("Mean - int32, window size 2, horizon 1, min window size 2") {    
     NS::Featurizers::AnalyticalRollingWindowTransformer<std::int32_t>                transformer(2, NS::Featurizers::AnalyticalRollingWindowCalculation::Mean, 1, 2);
 
-    NS::TestHelpers::FuzzyCheck(transformer.execute(1), {std::nan("")});
+    CHECK(NS::TestHelpers::FuzzyCheck(transformer.execute(1), {std::nan("")}));
 
-    NS::TestHelpers::FuzzyCheck(transformer.execute(2), {std::nan("")});
+    CHECK(NS::TestHelpers::FuzzyCheck(transformer.execute(2), {std::nan("")}));
 
-    NS::TestHelpers::FuzzyCheck(transformer.execute(3), {1.5});
+    CHECK(NS::TestHelpers::FuzzyCheck(transformer.execute(3), {1.5}));
 
-    NS::TestHelpers::FuzzyCheck(transformer.execute(4), {2.5});
+    CHECK(NS::TestHelpers::FuzzyCheck(transformer.execute(4), {2.5}));
 }
 
 TEST_CASE("Mean - int32, window size 1, horizon 2") {
     NS::Featurizers::AnalyticalRollingWindowTransformer<std::int32_t>                transformer(1, NS::Featurizers::AnalyticalRollingWindowCalculation::Mean, 2);
 
-    NS::TestHelpers::FuzzyCheck(transformer.execute(1), {std::nan(""), std::nan("")});
+    CHECK(NS::TestHelpers::FuzzyCheck(transformer.execute(1), {std::nan(""), std::nan("")}));
 
-    NS::TestHelpers::FuzzyCheck(transformer.execute(2), {std::nan(""), 1.0});
+    CHECK(NS::TestHelpers::FuzzyCheck(transformer.execute(2), {std::nan(""), 1.0}));
 
-    NS::TestHelpers::FuzzyCheck(transformer.execute(3), {1.0, 2.0});
+    CHECK(NS::TestHelpers::FuzzyCheck(transformer.execute(3), {1.0, 2.0}));
 
-    NS::TestHelpers::FuzzyCheck(transformer.execute(4), {2.0, 3.0});
+    CHECK(NS::TestHelpers::FuzzyCheck(transformer.execute(4), {2.0, 3.0}));
 
 }
 
 TEST_CASE("Mean - int32, window size 2, horizon 2") {
     NS::Featurizers::AnalyticalRollingWindowTransformer<std::int32_t>                transformer(2, NS::Featurizers::AnalyticalRollingWindowCalculation::Mean, 2);
 
-    NS::TestHelpers::FuzzyCheck(transformer.execute(1), {std::nan(""), std::nan("")});
+    CHECK(NS::TestHelpers::FuzzyCheck(transformer.execute(1), {std::nan(""), std::nan("")}));
 
-    NS::TestHelpers::FuzzyCheck(transformer.execute(2), {std::nan(""), 1.0});
+    CHECK(NS::TestHelpers::FuzzyCheck(transformer.execute(2), {std::nan(""), 1.0}));
 
-    NS::TestHelpers::FuzzyCheck(transformer.execute(3), {1.0, 1.5});
+    CHECK(NS::TestHelpers::FuzzyCheck(transformer.execute(3), {1.0, 1.5}));
 
-    NS::TestHelpers::FuzzyCheck(transformer.execute(3), {1.5, 2.5});
+    CHECK(NS::TestHelpers::FuzzyCheck(transformer.execute(3), {1.5, 2.5}));
 }
 
 TEST_CASE("Mean - int32, window size 2, horizon 2, min window size 2") {
     NS::Featurizers::AnalyticalRollingWindowTransformer<std::int32_t>                transformer(2, NS::Featurizers::AnalyticalRollingWindowCalculation::Mean, 2, 2);
 
-    NS::TestHelpers::FuzzyCheck(transformer.execute(1), {std::nan(""), std::nan("")});
+    CHECK(NS::TestHelpers::FuzzyCheck(transformer.execute(1), {std::nan(""), std::nan("")}));
 
-    NS::TestHelpers::FuzzyCheck(transformer.execute(2), {std::nan(""), std::nan("")});
+    CHECK(NS::TestHelpers::FuzzyCheck(transformer.execute(2), {std::nan(""), std::nan("")}));
 
-    NS::TestHelpers::FuzzyCheck(transformer.execute(3), {std::nan(""), 1.5});
+    CHECK(NS::TestHelpers::FuzzyCheck(transformer.execute(3), {std::nan(""), 1.5}));
 
-    NS::TestHelpers::FuzzyCheck(transformer.execute(4), {1.5, 2.5});
+    CHECK(NS::TestHelpers::FuzzyCheck(transformer.execute(4), {1.5, 2.5}));
 }
 
 TEST_CASE("Estimator Mean - int32, window size 2, horizon 2, min window size 2") {
@@ -103,16 +103,16 @@ TEST_CASE("Estimator Mean - int32, window size 2, horizon 2, min window size 2")
     );
 
     transformer->execute(1, callback);
-    NS::TestHelpers::FuzzyCheck(output[0], {std::nan(""), std::nan("")});
+    CHECK(NS::TestHelpers::FuzzyCheck(output[0], {std::nan(""), std::nan("")}));
 
     transformer->execute(2, callback);
-    NS::TestHelpers::FuzzyCheck(output[1], {std::nan(""), std::nan("")});
+    CHECK(NS::TestHelpers::FuzzyCheck(output[1], {std::nan(""), std::nan("")}));
 
     transformer->execute(3, callback);
-    NS::TestHelpers::FuzzyCheck(output[2], {std::nan(""), 1.5});
+    CHECK(NS::TestHelpers::FuzzyCheck(output[2], {std::nan(""), 1.5}));
 
     transformer->execute(4, callback);
-    NS::TestHelpers::FuzzyCheck(output[3], {1.5, 2.5});
+    CHECK(NS::TestHelpers::FuzzyCheck(output[3], {1.5, 2.5}));
 }
 
 using GrainType = std::vector<std::string>;
@@ -121,11 +121,11 @@ TEST_CASE("Grained Mean - 1 grain, window size 1, horizon 1") {
     NS::AnnotationMapsPtr                   pAllColumnAnnotations(NS::CreateTestAnnotationMapsPtr(1));
     NS::Featurizers::GrainedAnalyticalRollingWindowEstimator<std::int32_t>      estimator(pAllColumnAnnotations, 1, NS::Featurizers::AnalyticalRollingWindowCalculation::Mean, 1);
 
-    using GrainedInputType = std::tuple<GrainType const &, std::int32_t const &>;
+    using GrainedInputType = std::tuple<GrainType, std::int32_t>;
 
     const GrainType grain({"one"});
     const GrainedInputType tup1 = std::make_tuple(grain, 1);
-    const std::vector<GrainedInputType> vec = {tup1};
+    const std::vector<std::tuple<std::vector<std::string> const &, std::int32_t const &>> vec = {tup1};
 
 
     NS::TestHelpers::Train(estimator, vec);
@@ -139,27 +139,27 @@ TEST_CASE("Grained Mean - 1 grain, window size 1, horizon 1") {
     );
 
     transformer->execute(tup1, callback);
-    NS::TestHelpers::FuzzyCheck(output[0], {std::nan("")});
+    CHECK(NS::TestHelpers::FuzzyCheck(output[0], {std::nan("")}));
 
     const GrainedInputType tup2 = std::make_tuple(grain, 2);
 
     transformer->execute(tup2, callback);
-    NS::TestHelpers::FuzzyCheck(output[1], {1.0});
+    CHECK(NS::TestHelpers::FuzzyCheck(output[1], {1.0}));
 
     const GrainedInputType tup3 = std::make_tuple(grain, 3);
     transformer->execute(tup3, callback);
-    NS::TestHelpers::FuzzyCheck(output[2], {2.0});
+    CHECK(NS::TestHelpers::FuzzyCheck(output[2], {2.0}));
 }
 
 TEST_CASE("Grained Mean - 1 grain, window size 2, horizon 2, min window size 2") {
     NS::AnnotationMapsPtr                   pAllColumnAnnotations(NS::CreateTestAnnotationMapsPtr(1));
     NS::Featurizers::GrainedAnalyticalRollingWindowEstimator<std::int32_t>      estimator(pAllColumnAnnotations, 2, NS::Featurizers::AnalyticalRollingWindowCalculation::Mean, 2, 2);
 
-    using GrainedInputType = std::tuple<GrainType const &, std::int32_t const &>;
+    using GrainedInputType = std::tuple<GrainType, std::int32_t>;
 
     const GrainType grain({"one"});
     const GrainedInputType tup1 = std::make_tuple(grain, 1);
-    const std::vector<GrainedInputType> vec = {tup1};
+    const std::vector<std::tuple<std::vector<std::string> const &, std::int32_t const &>> vec = {tup1};
 
 
     NS::TestHelpers::Train(estimator, vec);
@@ -173,19 +173,19 @@ TEST_CASE("Grained Mean - 1 grain, window size 2, horizon 2, min window size 2")
     );
 
     transformer->execute(tup1, callback);
-    NS::TestHelpers::FuzzyCheck(output[0], {std::nan(""), std::nan("")});
+    CHECK(NS::TestHelpers::FuzzyCheck(output[0], {std::nan(""), std::nan("")}));
 
     const GrainedInputType tup2 = std::make_tuple(grain, 2);
     transformer->execute(tup2, callback);
-    NS::TestHelpers::FuzzyCheck(output[1], {std::nan(""), std::nan("")});
+    CHECK(NS::TestHelpers::FuzzyCheck(output[1], {std::nan(""), std::nan("")}));
 
     const GrainedInputType tup3 = std::make_tuple(grain, 3);
     transformer->execute(tup3, callback);
-    NS::TestHelpers::FuzzyCheck(output[2], {std::nan(""), 1.5});
+    CHECK(NS::TestHelpers::FuzzyCheck(output[2], {std::nan(""), 1.5}));
 
     const GrainedInputType tup4 = std::make_tuple(grain, 4);
     transformer->execute(tup4, callback);
-    NS::TestHelpers::FuzzyCheck(output[3], {1.5, 2.5});
+    CHECK(NS::TestHelpers::FuzzyCheck(output[3], {1.5, 2.5}));
 }
 
 TEST_CASE("Grained Mean - 2 grain, window size 2, horizon 2, min window size 1") {
@@ -194,8 +194,8 @@ TEST_CASE("Grained Mean - 2 grain, window size 2, horizon 2, min window size 1")
 
     using GrainedInputType = std::tuple<GrainType, std::int32_t>;
 
-    const std::vector<std::string> grainOne({"one"});
-    const std::vector<std::string> grainTwo({"two"});
+    const GrainType grainOne({"one"});
+    const GrainType grainTwo({"two"});
     const GrainedInputType tup1 = std::make_tuple(grainOne, 1);
     const GrainedInputType tup2 = std::make_tuple(grainTwo, 1);
     const std::vector<std::tuple<std::vector<std::string> const &, std::int32_t const &>> vec = {tup1, tup2};
@@ -212,21 +212,21 @@ TEST_CASE("Grained Mean - 2 grain, window size 2, horizon 2, min window size 1")
     );
 
     transformer->execute(tup1, callback);
-    NS::TestHelpers::FuzzyCheck(output[0], {std::nan(""), std::nan("")});
+    CHECK(NS::TestHelpers::FuzzyCheck(output[0], {std::nan(""), std::nan("")}));
 
 
     transformer->execute(tup2, callback);
-    NS::TestHelpers::FuzzyCheck(output[1], {std::nan(""), std::nan("")});
+    CHECK(NS::TestHelpers::FuzzyCheck(output[1], {std::nan(""), std::nan("")}));
 
 
     const GrainedInputType tup3 = std::make_tuple(grainOne, 2);
     transformer->execute(tup3, callback);
-    NS::TestHelpers::FuzzyCheck(output[2], {std::nan(""), 1});
+    CHECK(NS::TestHelpers::FuzzyCheck(output[2], {std::nan(""), 1}));
 
 
     const GrainedInputType tup4 = std::make_tuple(grainTwo, 2);
     transformer->execute(tup4, callback);
-    NS::TestHelpers::FuzzyCheck(output[3], {std::nan(""), 1});
+    CHECK(NS::TestHelpers::FuzzyCheck(output[3], {std::nan(""), 1}));
 }
 
 TEST_CASE("Serialization/Deserialization") {
