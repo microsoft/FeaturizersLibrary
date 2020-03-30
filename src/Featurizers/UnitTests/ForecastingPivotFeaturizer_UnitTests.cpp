90f0752c7eec16aa41887bc5121e98bb2b741924
wangyems <wangye@microsoft.com>
2020-03-30 12:01:29 -0700

    interfacing with raw buffers

--- src/Featurizers/UnitTests/ForecastingPivotFeaturizer_UnitTests.cpp
+++ src/Featurizers/UnitTests/ForecastingPivotFeaturizer_UnitTests.cpp
@@ -20,10 +20,12 @@ TEST_CASE("Invalid execute argument") {
     using InputType       = std::double_t;
     using NullableType    = NS::Traits<InputType>::nullable_type;
     using TransformedType = std::vector<InputType>;
-    using MatrixType      = NS::RowMajMatrix<NullableType>;
+    using MatrixType      = Eigen::Map<const NS::RowMajMatrix<NullableType>>;
 
-    MatrixType matrix1(3, 4);
-    MatrixType matrix2(2, 3);
+    NullableType matrix_data[]{0};
+
+    MatrixType matrix1(matrix_data, 3, 4);
+    MatrixType matrix2(matrix_data, 2, 3);
 
     std::vector<TransformedType> ret;
     auto const              callback(
@@ -31,7 +33,7 @@ TEST_CASE("Invalid execute argument") {
             ret.emplace_back(value);
         }
     );
-    
+
     CHECK_THROWS_WITH(NS::Featurizers::ForecastingPivotTransformer<InputType>().execute({}, callback), "There's no input matrix passed in!");
     CHECK_THROWS_WITH(NS::Featurizers::ForecastingPivotTransformer<InputType>().execute(NS::TestHelpers::make_vector<MatrixType>(matrix1, matrix2), callback), "All input matrixes should have the same number of columns!");
 }
@@ -41,25 +43,17 @@ TEST_CASE("One matrix of double") {
     using InputType       = std::double_t;
     using NullableType    = NS::Traits<InputType>::nullable_type;
     using TransformedType = std::vector<InputType>;
-    using MatrixType      = NS::RowMajMatrix<NullableType>;
-
-    MatrixType                                  matrix(3, 4);
+    using MatrixType      = Eigen::Map<const NS::RowMajMatrix<NullableType>>;
 
     // 1    4    6 null
     // 2    5 null null
     // 3 null null    7
-    matrix(0, 0) = 1;
-    matrix(0, 1) = 4;
-    matrix(0, 2) = 6;
-    matrix(0, 3) = NS::Traits<NullableType>::CreateNullValue();
-    matrix(1, 0) = 2;
-    matrix(1, 1) = 5;
-    matrix(1, 2) = NS::Traits<NullableType>::CreateNullValue();
-    matrix(1, 3) = NS::Traits<NullableType>::CreateNullValue();
-    matrix(2, 0) = 3;
-    matrix(2, 1) = NS::Traits<NullableType>::CreateNullValue();
-    matrix(2, 2) = NS::Traits<NullableType>::CreateNullValue();
-    matrix(2, 3) = 7;
+
+    NullableType matrixData[]{1, 4, 6, NS::Traits<NullableType>::CreateNullValue(),
+                              2, 5, NS::Traits<NullableType>::CreateNullValue(), NS::Traits<NullableType>::CreateNullValue(),
+                              3, NS::Traits<NullableType>::CreateNullValue(), NS::Traits<NullableType>::CreateNullValue(), 7};
+
+    MatrixType                                  matrix(matrixData, 3, 4);
 
     std::vector<TransformedType> ret;
     auto const              callback(
@@ -81,39 +75,26 @@ TEST_CASE("Two matrixes of string") {
     using InputType       = std::string;
     using NullableType    = NS::Traits<InputType>::nullable_type;
     using TransformedType = std::vector<InputType>;
-    using MatrixType      = NS::RowMajMatrix<NullableType>;
-
-    MatrixType                                  matrix1(3, 4);
-    MatrixType                                  matrix2(2, 4);
+    using MatrixType      = Eigen::Map<const NS::RowMajMatrix<NullableType>>;
 
     // matrix1
     // "one"   "six" "three"  "nine"
     // "two"  "four"  "five" "eight"
     //  null    null "seven"   "ten"
-    matrix1(0, 0) = "one";
-    matrix1(0, 1) = "six";
-    matrix1(0, 2) = "three";
-    matrix1(0, 3) = "nine";
-    matrix1(1, 0) = "two";
-    matrix1(1, 1) = "four";
-    matrix1(1, 2) = "five";
-    matrix1(1, 3) = "eight";
-    matrix1(2, 0) = NS::Traits<NullableType>::CreateNullValue();
-    matrix1(2, 1) = NS::Traits<NullableType>::CreateNullValue();
-    matrix1(2, 2) = "seven";
-    matrix1(2, 3) = "ten";
+
+    NullableType matrix1Data[]{"one", "six", "three", "nine",
+                               "two", "four", "five", "eight",
+                               NS::Traits<NullableType>::CreateNullValue(), NS::Traits<NullableType>::CreateNullValue(), "seven", "ten"};
 
     // matrix2
     //    "1"  null    "5"      "6"
     //    "2"  null    "3"      "4"
-    matrix2(0, 0) = "1";
-    matrix2(0, 1) = NS::Traits<NullableType>::CreateNullValue();
-    matrix2(0, 2) = "5";
-    matrix2(0, 3) = "6";
-    matrix2(1, 0) = "2";
-    matrix2(1, 1) = NS::Traits<NullableType>::CreateNullValue();
-    matrix2(1, 2) = "3";
-    matrix2(1, 3) = "4";
+
+    NullableType matrix2Data[]{"1", NS::Traits<NullableType>::CreateNullValue(), "5", "6",
+                               "2", NS::Traits<NullableType>::CreateNullValue(), "3", "4"};
+
+    MatrixType                                  matrix1(matrix1Data, 3, 4);
+    MatrixType                                  matrix2(matrix2Data, 2, 4);
 
     std::vector<TransformedType> ret;
     auto const              callback(
